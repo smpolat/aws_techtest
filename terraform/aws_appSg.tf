51ae4c6a2751e36345ba254050bb84f34001e03a
smpolat <mete@blackeaglenetwork.co.uk>
2022-04-20 13:14:36 +0100

    Added alb

---
+++ terraform/aws_appSg.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "appSg" {
+    vpc_id = "${aws_vpc.interview_vpc.id}"
+
+# HTTPS access from anywhere
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+# Outbound Rules
+# Internet access to anywhere
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "Application Tier Security Group"
+    }
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
