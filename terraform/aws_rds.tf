6fdb0bbf8b44215dbd03972ffe1b7df017efb76f
smpolat <mete@blackeaglenetwork.co.uk>
2022-04-20 12:56:27 +0100

    changed name-> db_name

--- terraform/aws_rds.tf
+++ terraform/aws_rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "db" {
     engine = "mysql"
     engine_version = "5.7"
     instance_class = "db.t3.micro"
-    name = var.db_name
+    db_name = var.db_name
     username = var.db_user
     password = var.db_pass
 }
