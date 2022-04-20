ec8f81b24e4366c72d4bce1b644d9f5aa5915144
smpolat <mete@blackeaglenetwork.co.uk>
2022-04-20 13:00:57 +0100

    added db subnet group

--- terraform/aws_rds.tf
+++ terraform/aws_rds.tf
@@ -6,4 +6,13 @@ resource "aws_db_instance" "db" {
     db_name = var.db_name
     username = var.db_user
     password = var.db_pass
+    db_subnet_group_name = aws_db_subnet_group.db.id
+}
+
+resource "aws_db_subnet_group" "db" {
+    name = "db_subnet_group"
+    subnet_ids = [
+        aws_subnet.database-subnet-1.id,
+        aws_subnet.database-subnet-2.id
+    ]
 }
